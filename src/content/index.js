ba84f7ce713ac4f4a53beea49717f6ce774ef455
Old-G <oldg590@gmail.com>
2021-04-28 22:41:04 +0300

    update stack info

--- src/content/index.js
+++ src/content/index.js
@@ -129,7 +129,7 @@ export default {
         alt: 'mongodb',
       },
     ],
-    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only`,
+    desc: `This is the Stack I have ever used. Every day I learn and develop, write clean code and drink a couple of cups of coffee 😉`,
   },
   contact: {
     title: 'Ping Me!',
